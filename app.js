ea527161527309e7bc26f45340bee730d9490f10
gitmail <gitmail@163.com>
2015-04-02 22:43:43 +0800

    config sth

--- app.js
+++ app.js
@@ -7,15 +7,18 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var ejs = require("ejs");
 
 var app = express();
 
 // view engine setup
+app.engine('.html', ejs.__express);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
